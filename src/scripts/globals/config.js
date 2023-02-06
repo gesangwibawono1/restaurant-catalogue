487919b6d8047adb32273b0b5ab2b803aa5815a4
Gesang Wibawono <gesangwibawono1@gmail.com>
2023-02-07 04:26:14 +0700

    modify icon

---
+++ src/scripts/globals/config.js
@@ -0,0 +1,12 @@
+const CONFIG = {
+  KEY: '094a5552a518cf8344e4e5c7be65850f',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+  CACHE_NAME: 'MovieCatalogue-V1',
+  DATABASE_NAME: 'movie-catalogue-database',
+  DATABASE_VERSION: 1,
+  OBJECT_STORE_NAME: 'movies',
+};
+
+export default CONFIG;
